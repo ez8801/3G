fae73e630e0ae36d40601d37d7964c917684e6a2
ez8801 <usd122@naver.com>
2017-05-30 20:12:31 +0900

    [Program] Add config table

---
+++ Client/Assets/Scripts/Data/Tables/ConfigTable.cs
@@ -0,0 +1,27 @@
+﻿using Data;
+using System;
+
+namespace Data
+{
+    [Serializable]
+    [PrimaryKey("index")]
+    public partial struct Config
+    {
+        public int index;
+        public string value;
+    }
+}
+
+public class ConfigTable : ArrayTable<Item>
+{
+    private static ConfigTable m_instance = null;
+    public static ConfigTable Instance
+    {
+        get
+        {
+            if (m_instance == null)
+                m_instance = new ConfigTable();
+            return m_instance;
+        }
+    }
+}
