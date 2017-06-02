a8bf43a3175351f747641bf84c6691e490566c7a
kjk1360 <kylin3163@gmail.com>
2017-06-03 05:56:00 +0900

    Add files via upload

---
+++ Client/Assets/Scripts/Unit/playerRig.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerRig : MonoBehaviour {
+    public GameObject explosionPrefab;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag.Equals("TestEnemy"))
+        {
+            Instantiate(explosionPrefab, other.transform.position, Quaternion.identity);
+            Destroy(other.gameObject);
+            gameEnd.killNumber++;
+        }
+    }
+
+
+
+}
