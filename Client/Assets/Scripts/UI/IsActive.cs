ebbb684ebd62726d341f4534cf3684c0a31f5f3f
kjk1360 <kylin3163@gmail.com>
2017-06-03 05:53:38 +0900

    Add files via upload

---
+++ Client/Assets/Scripts/UI/IsActive.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//단순하게 버튼 끄고 킴, 차후 상태머신 만들어야함.
+
+public class IsActive : MonoBehaviour {
+    
+    public GameObject button1;
+    public GameObject button2;
+    public GameObject button3;
+    public GameObject button4;
+    public void isclickOpen()
+    {
+        button1.GetComponent<UIButton>().isEnabled = false;
+        button2.GetComponent<UIButton>().isEnabled = false;
+        button3.GetComponent<UIButton>().isEnabled = false;
+        button4.GetComponent<UIButton>().isEnabled = false;
+    }
+
+    public void isclickClose()
+    {
+        button1.GetComponent<UIButton>().isEnabled = true;
+        button2.GetComponent<UIButton>().isEnabled = true;
+        button3.GetComponent<UIButton>().isEnabled = true;
+        button4.GetComponent<UIButton>().isEnabled = true;
+    }
+}
---
+++ Client/Assets/Scripts/UI/IsOpen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsOpen : MonoBehaviour {
+
+    public GameObject Target;
+    public void openUI()
+    {
+        Target.SetActive(true);
+    }
+    public void downUI()
+    {
+        Target.SetActive(false);
+    }
+}
