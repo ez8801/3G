e4380f7760a8282cdaa3f68b799137dde4bd6c96
ez8801 <usd122@naver.com>
2017-06-03 15:08:34 +0900

    [Program] fix minor bug

--- Client/Assets/Scripts/Unit/playerRig.cs
+++ Client/Assets/Scripts/Unit/playerRig.cs
@@ -2,18 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerRig : MonoBehaviour {
+public class playerRig : MonoBehaviour
+{
     public GameObject explosionPrefab;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag.Equals("TestEnemy"))
         {
             Instantiate(explosionPrefab, other.transform.position, Quaternion.identity);
             Destroy(other.gameObject);
-            gameEnd.killNumber++;
+            // gameEnd.killNumber++;
         }
     }
-
-
-
 }
