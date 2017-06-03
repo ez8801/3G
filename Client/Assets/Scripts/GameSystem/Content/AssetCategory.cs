54ead66cd0a68b2841ef6d455c69f72026c075da
ez8801 <usd122@naver.com>
2017-06-03 16:11:32 +0900

    [Program] Add AssetCategory

---
+++ Client/Assets/Scripts/GameSystem/Content/AssetCategory.cs
@@ -0,0 +1,15 @@
+﻿/* 
+ * AssetCategory.cs
+ * 
+ * Writer : BMW
+ * Date   : 2017-06-03
+ * 
+ * Copyright ⓒ Util. DefaultCompany, All rights reserved
+ */
+
+public enum AssetCategory
+{
+    None,
+    Data,
+    UI,
+}
