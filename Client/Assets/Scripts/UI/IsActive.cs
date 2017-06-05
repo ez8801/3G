e4a2a9110a540064085d54f620e16db2fcd2b0aa
ez8801 <usd122@naver.com>
2017-06-06 03:09:48 +0900

    [Program] fix minor bug, modify project version

--- Client/Assets/Scripts/UI/IsActive.cs
+++ Client/Assets/Scripts/UI/IsActive.cs
@@ -12,17 +12,17 @@ public class IsActive : MonoBehaviour {
     public GameObject button4;
     public void isclickOpen()
     {
-        button1.GetComponent<UIButton>().isEnabled = false;
-        button2.GetComponent<UIButton>().isEnabled = false;
-        button3.GetComponent<UIButton>().isEnabled = false;
-        button4.GetComponent<UIButton>().isEnabled = false;
+        //button1.GetComponent<UIButton>().isEnabled = false;
+        //button2.GetComponent<UIButton>().isEnabled = false;
+        //button3.GetComponent<UIButton>().isEnabled = false;
+        //button4.GetComponent<UIButton>().isEnabled = false;
     }
 
     public void isclickClose()
     {
-        button1.GetComponent<UIButton>().isEnabled = true;
-        button2.GetComponent<UIButton>().isEnabled = true;
-        button3.GetComponent<UIButton>().isEnabled = true;
-        button4.GetComponent<UIButton>().isEnabled = true;
+        //button1.GetComponent<UIButton>().isEnabled = true;
+        //button2.GetComponent<UIButton>().isEnabled = true;
+        //button3.GetComponent<UIButton>().isEnabled = true;
+        //button4.GetComponent<UIButton>().isEnabled = true;
     }
 }
