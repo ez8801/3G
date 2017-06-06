35444b390571722eabcdc241cda9993c470e9980
ez8801 <usd122@naver.com>
2017-06-06 10:57:26 +0900

    [Program] Add Text stuff

---
+++ Client/Assets/Scripts/System/Text/ControlChars.cs
@@ -0,0 +1,48 @@
+﻿/* 
+ * ControlChars.cs
+ * 
+ * Writer : EZ
+ * Date   : 2017-06-06
+ * 
+ * Copyright ⓒ Util. DefaultCompany, All rights reserved
+ */
+
+using UnityEngine;
+
+public static class ControlChars
+{
+    public enum LineEnding
+    {
+        NONE,
+        WINDOWS,            // CRLF
+        MACINTOSH,          // CR
+        UNIX,               // LF
+        MIXED               // MIXED
+    }
+
+    public const char Back = '\b';
+    public const char Cr = '\r';
+    public const string CrLf = "\r\n";
+    public const char FormFeed = '\f';
+    public const char Lf = '\n';
+    public const string NewLine = "\r\n";
+    public const char NullChar = '\0';
+    public const char Quote = '"';
+    public const char Tab = '\t';
+    public const char VerticalTab = '\v';
+
+    public static LineEnding CurrentSetting
+    {
+        get
+        {
+            if (System.Environment.NewLine.Equals(Cr))
+                return LineEnding.MACINTOSH;
+            else if (System.Environment.NewLine.Equals(CrLf))
+                return LineEnding.WINDOWS;
+            else if (System.Environment.NewLine.Equals(Lf))
+                return LineEnding.UNIX;
+
+            return LineEnding.NONE;
+        }
+    }
+}
---
+++ Client/Assets/Scripts/System/Text/StringEx.cs
@@ -0,0 +1,60 @@
+﻿/* 
+ * StringEx.cs
+ * 
+ * Writer : EZ
+ * Date   : 2017-06-06
+ * 
+ * Copyright ⓒ Util. DefaultCompany, All rights reserved
+ */
+ 
+using System.Text;
+
+public static class StringEx
+{
+    private static StringBuilder m_builder = new StringBuilder();
+
+    /// <summary>
+    /// 서식 문자열을 처리한 문자열을 반환합니다.
+    /// </summary>
+    public static string Format(string format, params object[] args)
+    {
+        if (string.IsNullOrEmpty(format))
+            return string.Empty;
+
+        m_builder.Length = 0;
+        m_builder.AppendFormat(format, args);
+        return m_builder.ToString();
+    }
+
+    /// <summary>
+    /// 지정된 String 배열의 요소를 연결합니다.
+    /// </summary>
+    public static string Concat(params string[] args)
+    {
+        if (args == null || args.Length == 0)
+            return string.Empty;
+
+        m_builder.Length = 0;
+        for (int i = 0; i < args.Length; i++)
+        {
+            m_builder.Append(args[i]);
+        }
+        return m_builder.ToString();
+    }
+
+    /// <summary>
+    /// 지정된 Object 배열에 있는 요소의 문자열 표현을 연결합니다.
+    /// </summary>
+    public static string Concat(params object[] args)
+    {
+        if (args == null || args.Length == 0)
+            return string.Empty;
+
+        m_builder.Length = 0;
+        for (int i = 0; i < args.Length; i++)
+        {
+            m_builder.Append(args[i].ToString());
+        }
+        return m_builder.ToString();
+    }
+}
