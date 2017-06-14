9b1bdc2b0e9b8b1c128bef70d7038fe8371d8592
soongsae <lsj616hi@gmail.com>
2017-06-14 13:21:59 +0900

    김기린 코드 Git에 반영 -수정자 이승준

--- Client/Assets/Scripts/Unit/playerRig.cs
+++ Client/Assets/Scripts/Unit/playerRig.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerRig : MonoBehaviour
-{
+public class playerRig : MonoBehaviour {
     public GameObject explosionPrefab;
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag.Equals("TestEnemy"))
         {
             Instantiate(explosionPrefab, other.transform.position, Quaternion.identity);
             Destroy(other.gameObject);
-            // gameEnd.killNumber++;
+            gameEnd.killNumber++;
         }
     }
+
+
+
 }
